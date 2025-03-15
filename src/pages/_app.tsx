930419283b0acdaed9c5a596ef172952c6253d48
nigussolomon <nigussolomon444@gmail.com>
2025-03-15 08:34:16 +0300

    setup mantine configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,51 @@
 import "@/styles/globals.css";
+import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
+
 import type { AppProps } from "next/app";
+import {
+  createTheme,
+  MantineProvider,
+  MantineColorsTuple,
+} from "@mantine/core";
+
+import { DM_Sans } from "next/font/google";
+import { Notifications } from "@mantine/notifications";
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+  variable: "--font-dm-sans",
+});
+
+const base: MantineColorsTuple = [
+  "#fbefef",
+  "#efdcdc",
+  "#e2b4b4",
+  "#d68a8a",
+  "#cc6667",
+  "#c65050",
+  "#c44544",
+  "#ad3736",
+  "#9b2f2f",
+  "#541718",
+];
+
+const theme = createTheme({
+  colors: {
+    primary: base,
+  },
+  primaryColor: "primary",
+  primaryShade: { light: 9, dark: 9 },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider defaultColorScheme="light" theme={theme}>
+      <Notifications />
+      <main className={dmSans.className}>
+        <Component {...pageProps} />
+      </main>
+    </MantineProvider>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
+import { ColorSchemeScript, mantineHtmlProps } from "@mantine/core";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html lang="en" {...mantineHtmlProps}>
+      <Head>
+        <ColorSchemeScript defaultColorScheme="light" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
